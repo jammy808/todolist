e1636957765f33377129b92cf98a87b71067b561
soham <soahm@gmail.com>
2024-01-06 13:21:45 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -12,6 +12,16 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        'right' :{
+          '0%':{transform : 'translateX(-70px)'},
+          '100%':{transform : 'translateX(0)'}
+        }
+    },
+    animation:{
+      'right':'right 0.3s ease-in-out'
+    },
+  
     },
   },
   plugins: [],
